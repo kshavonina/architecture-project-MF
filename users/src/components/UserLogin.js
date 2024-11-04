f2653fdd31def4a5f112b91dbce6dca829f2e55f
Kseniia Shavonina <kshavonina@yandex.ru>
2024-11-04 21:48:05 +0300

    Add components to hosts

--- users/src/components/UserLogin.js
+++ users/src/components/UserLogin.js
@@ -1,9 +1,17 @@
 import { useState } from 'react';
 import './UserLogin.css';
+import api from '../utils/api'
 
-export default function UserLogin({ onLogin }) {
+export default function UserLogin() { // Убрали onLogin из параметров
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    
+    // Добавили onLogin в UserLogin и отправляем полученный jwt с помощью события
+    function onLogin(name, password) {
+        dispatchEvent(new CustomEvent("jwt-change", {
+            detail: api.login(name, password)
+        }) );
+    }
 
     return (
         <div className='login-box'>
