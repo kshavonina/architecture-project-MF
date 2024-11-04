ebd587b2c821207b4b609e6953df45c80a79e99f
Kseniia Shavonina <kshavonina@yandex.ru>
2024-11-04 21:33:02 +0300

    Specialized api for users and tasks

--- users/src/utils/api.js
+++ users/src/utils/api.js
@@ -3,6 +3,18 @@ class Api {
         // стандартная реализация — объект options
         this._address = address;
     }
+
+    login(username, password) {
+        // В реальном приложении здесь будет обращение к API login service
+        return 'fake-token';
+    }
+
+    getProfile() {
+        // В реальном приложении здесь будет обращение к API login service
+        return {
+            userName: 'Студент',
+        };
+    }
 }
 
 const api = new Api({
